3e1223c8c8c857e1bdf794df4c762eb44e1d0712
tattepr <mild.4112@gmail.com>
2022-02-16 06:34:32 +0700

    Prettier code

--- src/routers/admin.py
+++ src/routers/admin.py
@@ -1,4 +1,3 @@
-from contextlib import nullcontext
 from fastapi import APIRouter, Depends, HTTPException
 from ..schemas import Product
 from ..auth import AuthHandler
@@ -13,99 +12,104 @@ router = APIRouter(
    responses={404: {"message": "Not found"}}
 )
 
-myclient = MongoClient("localhost",27017)
+myclient = MongoClient("localhost", 27017)
 db = myclient["Yaya-Fever"]
 plants = db["plants"]
 users = db["users"]
 
-def check_permission(username : str) -> bool:
-   result = users.find_one({"username" : username})
-   return result["permission"] == 1
+
+def check_permission(username: str) -> bool:
+    result = users.find_one({"username": username})
+    return result["permission"] == 1
 
 
 @router.get('/plant_info/{id}')
-def get_plant_info(id :int, username = Depends(auth_handler.auth_wrapper)):
+def get_plant_info(id: int, username=Depends(auth_handler.auth_wrapper)):
     """Show plant detail"""
-    query = {"ID":id}
+    query = {"ID": id}
     detail = {
-        "_id":0,
-        "plant_name":1,
-        "humidity_soil_hard":1,
-        "humidity_air_hard":1,
-        "height_hard":1,
-        "temp":1,
-        "activate_auto":1
+        "_id": 0,
+        "plant_name": 1,
+        "humidity_soil_hard": 1,
+        "humidity_air_hard": 1,
+        "height_hard": 1,
+        "temp": 1,
+        "activate_auto": 1
     }
-    result = plants.find_one(query , detail)
-    if (result != None) and check_permission(username):
+    result = plants.find_one(query, detail)
+    if (result is not None) and check_permission(username):
         return result
-    elif (result == None) and check_permission(username): 
+    elif (result is None) and check_permission(username):
         raise HTTPException(status_code=403, detail="Plant ID not found")
-    elif not check_permission(username) :
+    elif not check_permission(username):
         raise HTTPException(status_code=401, detail='Permission denined')
 
+
 @router.post("/auto_mode")
-def auto_mode(product : Product, username = Depends(auth_handler.auth_wrapper)):
+def auto_mode(product: Product, username=Depends(auth_handler.auth_wrapper)):
     """Auto mode on/off."""
     result = plants.find_one({"ID": product.ID})
-    if (result != None) and check_permission(username):
+    if (result is not None) and check_permission(username):
         query = {"ID": product.ID}
-        new = {"$set" : {"activate_auto": product.activate_auto}}
-        plants.update_one(query,new)
+        new = {"$set": {"activate_auto": product.activate_auto}}
+        plants.update_one(query, new)
         return {
            "success"
         }
-    elif not check_permission(username) :
+    elif not check_permission(username):
         raise HTTPException(status_code=401, detail='Permission denined')
 
+
 @router.post("/humidity_front_want")
-def humidity_front_want(product : Product, username = Depends(auth_handler.auth_wrapper)):
+def humidity_front_want(product: Product, username=Depends(auth_handler.auth_wrapper)):
     """Set humidity to Hardware."""
     result = plants.find_one({"ID": product.ID})
-    if (result != None) and check_permission(username):
+    if (result is not None) and check_permission(username):
         query = {"ID": product.ID}
-        new = {"$set" : {"humidity_soil_front": product.humidity_soil_front}}
-        plants.update_one(query,new)
+        new = {"$set": {"humidity_soil_front": product.humidity_soil_front}}
+        plants.update_one(query, new)
         return {
             "updated success"
         }
-    elif not check_permission(username) :
+    elif not check_permission(username):
         raise HTTPException(status_code=401, detail='Permission denined')
 
-@router.post("/new_plant") 
-def new_plant(product : Product , username = Depends(auth_handler.auth_wrapper)):
+
+@router.post("/new_plant")
+def new_plant(product: Product, username=Depends(auth_handler.auth_wrapper)):
     """If It's new ID add new plant If that ID already have update plant"""
     result = plants.find_one({"ID": product.ID})
-    if (result == None) and check_permission(username):
+    if (result is None) and check_permission(username):
         x = jsonable_encoder(product)
         plants.insert_one(x)
         return {
             "added success"
         }
-    elif (result != None ) and check_permission(username):
-        query = {"ID":product.ID}
+    elif (result is not None) and check_permission(username):
+        query = {"ID": product.ID}
         new = {"$set": {
             "plant_name": product.plant_name,
             "detail": product.detail,
-            "price":product.price,
-            "ID":product.ID
+            "price": product.price,
+            "ID": product.ID
         }}
         plants.update_one(query, new)
         return {
             "Updated success"
         }
-    elif not check_permission(username) :
+    elif not check_permission(username):
         raise HTTPException(status_code=401, detail='Permission denined')
-        
+
+
 @router.delete("/delete_plant")
-def delete_plant(product : Product , username = Depends(auth_handler.auth_wrapper)):
+def delete_plant(product: Product, username=Depends(auth_handler.auth_wrapper)):
     """Delete plants"""
     result = plants.find_one({"ID": product.ID})
-    if (result != None) and check_permission(username):
+    if (result is not None) and check_permission(username):
         query = {"ID": product.ID}
         plants.delete_one(query)
         return {
             "delete success"
         }
-    elif not check_permission(username) :
+    elif not check_permission(username):
         raise HTTPException(status_code=401, detail='Permission denined')
